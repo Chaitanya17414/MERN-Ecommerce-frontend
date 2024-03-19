564d33b6e082cd49c3e074010a0e9dc555c21560
Chaitanyachinta17414 <chaitanyachinta17414@gmail.com>
2024-03-19 10:55:35 +0530

    lapto view changes

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -44,7 +44,7 @@ function Navbar() {
                         </svg></span>
                 </label>
             </div>
-            <div className="grid grid-cols-2 col-start-8 sm:hidden">
+            <div className="grid grid-cols-2 col-start-8 sm:hidden md:hidden">
                 <div className="group flex items-center justify-center text-white">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
@@ -100,7 +100,7 @@ function Navbar() {
                     <span className="rounded-lg p-2 bg-red-500">{cartTotalQuantity}</span>
                 </div></Link>
             </div>
-            <div className="grid-cols-2 col-end-8 col-span-2 hidden sm:grid">
+            <div className="grid-cols-2 col-end-8 col-span-2 hidden sm:grid md:grid">
                 <div className="group flex items-center justify-center text-white">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
